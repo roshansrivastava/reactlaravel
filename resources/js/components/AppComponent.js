4f2d7a274a8f37ecf5a6b5b17444beea0f9b61be
“roshansrivastava” <roshan.srivastava@webnyxa.tech>
2022-05-03 21:33:49 +0530

    route protected is done

--- resources/js/components/AppComponent.js
+++ resources/js/components/AppComponent.js
@@ -1,8 +1,8 @@
-import React, { Component } from 'react';
+import React, { useEffect } from 'react';
 import ReactDOM from 'react-dom';
 import '../../css/app.css';
 import Registration from './Registration';
-import Example  from './Example';
+import Example from './Example';
 import Login from './Login';
 import ForgetPassword from './ForgetPassword';
 import {
@@ -23,35 +23,44 @@ import PurchaseBasic from '../layout/PurchaseBasic';
 import PurchaseFree from '../layout/PurchaseFree';
 import ResetPassword from './ResetPassword'
 import AddingUser from '../layout/AddingUser';
-import Protected from './Protected';
-export default class AppComponent extends Component {
-  render() {
-    return (
-      <>
+import PageNotFound from './PageNotFound';
+export default function AppComponent() {
+  let data = JSON.parse(localStorage.getItem('user'));
+  useEffect(() => {
+    console.log('tt', data);
+  })
+  return (
+    <>
       <Router>
-          <Routes>
-              <Route exact path='/'element={<Registration/>}></Route>
-              <Route exact path='/login' element={<Login/>}></Route>
-              <Route exact path='/forget/password'element={<ForgetPassword/>}></Route>
-              <Route exact path='/dashboard'element={<Dashboard/>}></Route>
-              <Route exact path='/dashboard/user'element={<User/>}></Route>
-              <Route exact path='/dashboard/music'element={<Music/>}></Route>
-              <Route exact path='/dashboard/adduser'element={<AddUser/>}></Route>
-              <Route exact path='/dashboard/updateuser/:id'element={<EditUser/>}></Route>
-              <Route exact path='/dashboard/home'>
-              <Protected Cmp = {Home}/></Route>
-              <Route exact path="/dashboard/purchase/premium"element={<PurchasePremium/>}></Route>
-              <Route exact path="/dashboard/purchase/basic"element={<PurchaseBasic/>}></Route>
-              <Route exact path='/dashboard/purchase/free'element={<PurchaseFree/>}></Route> 
-              <Route exact path='/reset/password/:slug'element={<ResetPassword/>}></Route>
-              <Route exact path='/addinguser'element={<AddingUser/>}></Route>
-          </Routes> 
+        <Routes>
+          {data ? (
+            <>
+
+              <Route exact path='/dashboard/home' element={<Home />}></Route>
+              <Route exact path='/forget/password' element={<ForgetPassword />}></Route>
+              <Route exact path='/dashboard' element={<Dashboard />}></Route>
+              <Route exact path='/dashboard/user' element={<User />}></Route>
+              <Route exact path='/dashboard/music' element={<Music />}></Route>
+              <Route exact path='/dashboard/adduser' element={<AddUser />}></Route>
+              <Route exact path='/dashboard/updateuser/:id' element={<EditUser />}></Route>
+              <Route exact path="/dashboard/purchase/premium" element={<PurchasePremium />}></Route>
+              <Route exact path="/dashboard/purchase/basic" element={<PurchaseBasic />}></Route>
+              <Route exact path='/dashboard/purchase/free' element={<PurchaseFree />}></Route>
+            </>
+          ) : (
+            <>              <Route exact path='/' element={<Registration />}></Route>
+              <Route exact path='/login' element={<Login />}></Route>
+              <Route exact path='/reset/password/:slug' element={<ResetPassword />}></Route>
+              <Route exact path='/addinguser' element={<AddingUser />}></Route>
+            </>
+          )}
+          <Route element={PageNotFound}/>
+        </Routes>
       </Router>
-      </>
-    )
-  }
+    </>
+  );
 }
 
 if (document.getElementById('example')) {
-    ReactDOM.render(<AppComponent />, document.getElementById('example'));
+  ReactDOM.render(<AppComponent />, document.getElementById('example'));
 }
---
+++ resources/js/components/PageNotFound.js
@@ -0,0 +1,19 @@
+import React from 'react';
+// import { useNavigate } from 'react-router-dom';
+export default function PageNotFound() {
+  // const navigate = useNavigate();
+  //   let Cmp = props.Cmp;
+  //   useEffect (()=>{
+  //       if(!localStorage.getItem('user')){
+  //         navigate('/login')
+  //       }
+  //   })
+  return (
+    <div
+      style={{ height: "100vh", backgroundColor: "#4577ab" }}
+      className="d-flex justify-content-center align-items-center"
+    >
+      Page Not Found
+    </div>
+  )
+}
